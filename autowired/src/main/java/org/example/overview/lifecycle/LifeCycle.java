386f478829771e99553a06f17a965e68cb214a41
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-11-02 13:57:39 +0900

    22.11.02

--- autowired/src/main/java/org/example/overview/lifecycle/LifeCycle.java
+++ autowired/src/main/java/org/example/overview/lifecycle/LifeCycle.java
@@ -31,7 +31,9 @@ public class LifeCycle implements InitializingBean, DisposableBean, BeanNameAwar
 
     @Autowired
     public LifeCycle(AnotherLifeCycle anotherLifeCycle) {
+
         this.anotherLifeCycle = anotherLifeCycle;
+        //this.anotherLifeCycle = anotherLifeCycle;
         System.out.println("LifeCycle(AnotherLifeCycle anotherLifeCycle)");
     }
 
@@ -67,6 +69,4 @@ public class LifeCycle implements InitializingBean, DisposableBean, BeanNameAwar
     public void destroyMethod() {
         System.out.println(this.getClass().getName() + " destroyMethod()");
     }
-
-
 }
