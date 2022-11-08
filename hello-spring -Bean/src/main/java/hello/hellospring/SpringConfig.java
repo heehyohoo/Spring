1efe7d7c5a07b8c8d24811dfbd5ba1b2c4230cd9
Jay <myride.jay@gmail.com>
2022-11-08 19:14:14 +0900

    hello Spring

---
+++ hello-spring -Bean/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,22 @@
+package hello.hellospring;
+
+import hello.hellospring.repositroy.MemberRepository;
+import hello.hellospring.repositroy.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+}
